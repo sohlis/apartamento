9c0f4cc067600ee8d33fa9a8e57e3569a268c83d
Stefan Sohlstrom <stefan.sohlstrom@gmail.com>
2016-10-25 02:05:21 -0700

    👱🏢👍

--- README.md
+++ README.md
@@ -1,4 +1,8 @@
+# Apartamento
 
-
-Possible resources
-`https://github.com/cheeriojs/cheerio/blob/master/Readme.md`
+### Requires
+- [Ramda](http://ramdajs.com/)
+- [Cheerio](https://cheerio.js.org/)
+- Perseverance
+- Willingness to accept defeat
+- Large move-in deposit
--- index.js
+++ index.js
@@ -6,19 +6,13 @@ const options = {
     url: 'http://sfbay.craigslist.org/search/apa?search_distance=5&postal=94133&min_price=2000&max_price=4400&bedrooms=1&bathrooms=1&availabilityMode=0'
 };
 
-// timeRange is the distance from last posts to current time
-// 15819421 is about 5 hours behind now
-// Beware, this is last updated time, not first posting
-// TODO: figure out post date
-const timeRange = 7819421;
-
 function callback(error, response, body) {
   if (!error && response.statusCode == 200) {
     // This tells cherio to convert our body string into jQuery style parsable things
     $ = cheerio.load(body);
 
     // Initialize empty array to store our fun datums
-    let newArr = []
+    let listingDataArr = []
 
     for (var i = 0; i < $('.pl a').length; i++) {
     // for (var i = 0; i < 2; i++) { // i < 1 is for testing reasons only
@@ -49,27 +43,33 @@ function callback(error, response, body) {
 
       // Store it
       // Add all of these to a new Array which will store this combined data
-      newArr.push(tempArr);
+      listingDataArr.push(tempArr);
     }
-    // Log out the final result
-    // console.log(newArr);
 
-    newerArr = []
+    // timeRange is the distance from last posts to current time
+    // 15819421 is about 5 hours behind now
+    // Beware, this is last updated time, not first posting
+    // TODO: figure out post date
+    const timeRange = 7819421;
 
-    newArr.map(x => {
+    // She stores all our postings in the given time
+    let timeRangeArr = []
+
+    // Take all our listing data, see the posting time and compare to time range
+    // For listings that meet the criteria, add them to our new timeRangeArr
+    listingDataArr.map(x => {
       if (dateCompare(x[3], timeRange)) {
-        newerArr.push(x);
+        timeRangeArr.push(x);
       }
     });
 
-    console.log(newerArr);
-    console.log(newerArr.length);
+    // Log flume
+    console.log(timeRangeArr);
+    console.log(timeRangeArr.length);
   }
 }
 
 request(options, callback);
-
-
 
 function dateCompare(date, range) {
   if (Math.abs(Date.parse(date) - Date.now()) < range) {
@@ -79,6 +79,3 @@ function dateCompare(date, range) {
     return false
   }
 }
-
-
-  // dateCompare('2016-10-25 00:18', 3000000)

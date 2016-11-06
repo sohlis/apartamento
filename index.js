967e933907004c02c7c1302b4e6f5a6819f57da7
sohlis <stefan.sohlstrom@gmail.com>
2016-11-06 13:04:06 -0800

    refactoring

--- index.js
+++ index.js
@@ -2,48 +2,51 @@ const request = require('request');
 const cheerio = require('cheerio');
 const R = require('ramda');
 
+// Secrets stored in .env
 require('dotenv').config();
-
-var client = require('twilio')(process.env.ACCOUNTSID, process.env.AUTHTOKEN);
+const client = require('twilio')(process.env.ACCOUNTSID, process.env.AUTHTOKEN);
 
 const options = {
-    url: 'http://sfbay.craigslist.org/search/apa?search_distance=5&postal=94133&min_price=2000&max_price=4400&bedrooms=1&bathrooms=1&availabilityMode=0'
+    // Setup your search in craigslist to your liking
+    url: 'http://sfbay.craigslist.org/search/apa?query=-special+-%22Brand+new%22+-zipcar+-furnished&srchType=T&hasPic=1&search_distance=5&postal=94133&min_price=2000&max_price=4400&bedrooms=1&bathrooms=1&availabilityMode=0'
 };
 
-function callback(error, response, body) {
+const callback = (error, response, body) => {
   if (!error && response.statusCode == 200) {
-    // This tells cherio to convert our body string into jQuery style parsable things
+
+    // This tells cheerio to convert our body string into jQuery style parsable things
     $ = cheerio.load(body);
 
     // Initialize empty array to store our fun datums
     let listingDataArr = []
 
-    for (var i = 0; i < $('.pl a').length; i++) {
-    // for (var i = 0; i < 2; i++) { // i < 1 is for testing reasons only
+    // Iterate through every different .result-info instance in the DOM
+    for (var i = 0; i < $('.result-info').length; i++) {
       let tempArr = []
 
       // Title
       // This gets the title associated with each posting
-      tempArr.push(R.map(R.prop('data'), $('.pl > a')[i].children)[0])
+      tempArr.push(R.prop(`${i}`, $('.result-title')).children[0].data)
 
       // URL
       // This very confusing line gets the links associated with each posting
-      tempArr.push('http://sfbay.craigslist.org' + R.prop('href', $('.pl > a')[i].attribs));
+      tempArr.push(`http://sfbay.craigslist.org` + R.prop(`${i}`, $('.result-title')).children[0].parent.attribs.href)
 
       // Price
-      // Working right to left: the first $(".price") entry contains an object with
-      // the property 'children' which contains the property data that holds the
-      // price. So i am using nested R.prop to achieve this.
-      tempArr.push(R.prop('data', R.prop('children', $(".price")[i])[0]));
+      tempArr.push(R.prop(`${i}`, $('.result-price')).children[0].data)
 
       // Time
       // Grab the time, casually, using the <time datetime> attribute
-      tempArr.push($("time")[i].attribs.datetime)
+      tempArr.push(R.prop(`${i}`, $('.result-date')).attribs.datetime)
 
       // Location
-      // What an embaressingly long one! This basically takes the location in
-      // perens and removes (them) and returns just a nice location name
-      // tempArr.push($(".pnr small")[i].children[0].data.split('(')[1].split(')')[0]);
+      // Check that the location itself is there first, and then
+      // add it to the array
+      if (R.prop(`${i}`,$('.result-hood')) !== undefined) {
+        tempArr.push(R.prop(`${i}`,$('.result-hood')).children[0].data)
+      }  else {
+        tempArr.push('')
+      }
 
       // Store it
       // Add all of these to a new Array which will store this combined data
@@ -53,8 +56,9 @@ function callback(error, response, body) {
     // timeRange is the distance from last posts to current time
     // 15819421 is about 5 hours behind now
     // Beware, this is last updated time, not first posting
-    // TODO: figure out post date
-    const timeRange = 7819421;
+    // TODO: Construct a function that is more human readable
+    //       for timeRange
+    const timeRange = 1819421;
 
     // She stores all our postings in the given time
     let timeRangeArr = []
@@ -70,25 +74,26 @@ function callback(error, response, body) {
     // Text it
     // For every entry in timeRangeArr we are going to send a text using twilio
     timeRangeArr.map((x) => {
-      client.messages.create({
-          to: process.env.MYPHONENUMBER,
-          from: process.env.TWILIOPHONENUMBER,
-          body: x[0] + ` ` + x[1],
-      }, function(err, message) {
-          console.log(`- ` + message.body);
-      });
+      textMe(process.env.MYPHONENUMBER, process.env.TWILIOPHONENUMBER, x);
     })
   }
 }
 
-
 request(options, callback);
 
-function dateCompare(date, range) {
-  if (Math.abs(Date.parse(date) - Date.now()) < range) {
-    return true
-  }
-  else {
-    return false
-  }
+// dateCompare takes a given date and a range and compares Now and date
+// and returns bool
+const dateCompare = (date, range) => {
+    return Math.abs(Date.now() - Date.parse(date)) < range;
+}
+
+// textMe uses Twilio to text you the results
+const textMe = (myNumber, twilioNumber, x) => {
+  client.messages.create({
+      to: myNumber,
+      from: twilioNumber,
+      body: x[0] + ` ` + x[1],
+  }, function(err, message) {
+      console.log(`- ` + message.body);
+  });
 }
